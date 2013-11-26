3ce1f61d787a403625c28c17ad2c63d9cd62c6a4
Kevin Herrera <kevin@herrera.io>
2013-11-26 15:52:47 -0800

    Updating documentation.

--- src/lib/Phine/Phar/File/Reader.php
+++ src/lib/Phine/Phar/File/Reader.php
@@ -8,7 +8,25 @@ use Phine\Phar\Exception\FileException;
 /**
  * Manages strict file reading operations.
  *
+ * Summary
+ * -------
+ *
+ * The `Reader` class provides a way to read files with heavy error checking.
+ * Instead of subtly triggering warnings, exceptions are thrown if something
+ * could not be done (e.g. fseek(), fread() a specific number of bytes, etc).
+ *
+ * Starting
+ * --------
+ *
+ * To start, you will need to create a new instance of `Reader`.
+ *
+ *     use Phine\Phar\File\Reader;
+ *
+ *     $reader = new Reader('/path/to/file');
+ *
  * @author Kevin Herrera <kevin@herrera.io>
+ *
+ * @api
  */
 class Reader
 {
@@ -29,10 +47,22 @@ class Reader
     /**
      * Sets the path of the file for reading.
      *
+     * This method will set the path of the file to be read.
+     *
+     *     use Phine\Phar\File\Reader;
+     *
+     *     $reader = new Reader('/path/to/file');
+     *
+     * > Note that the file will not be opened until the first read operation
+     * > is attempted. An exception will still be thrown, however, if the file
+     * > does not exist.
+     *
      * @param string $file The path of the file.
      *
      * @throws Exception
      * @throws FileException If the file path is not valid.
+     *
+     * @api
      */
     public function __construct($file)
     {
@@ -48,6 +78,18 @@ class Reader
 
     /**
      * Closes the file if it's open.
+     *
+     * This method will close the file handle if it has been opened.
+     *
+     *     use Phine\Phar\File\Reader;
+     *
+     *     $reader = new Reader('/path/to/file');
+     *
+     *     $data = $reader->read(10);
+     *
+     *     unset($reader);
+     *
+     * @api
      */
     public function __destruct()
     {
@@ -59,7 +101,13 @@ class Reader
     /**
      * Returns the path of the file.
      *
+     * This method will return the path to the file being read.
+     *
+     *     $file = $reader->getFile();
+     *
      * @return string The path of the file.
+     *
+     * @api
      */
     public function getFile()
     {
@@ -69,10 +117,16 @@ class Reader
     /**
      * Returns the reading file handle.
      *
+     * This method will return the reading file handle for the file.
+     *
+     *     $handle = $reader->getHandle();
+     *
      * @return resource The file handle.
      *
      * @throws Exception
      * @throws FileException If the file could not be opened for reading.
+     *
+     * @api
      */
     public function getHandle()
     {
@@ -88,10 +142,16 @@ class Reader
     /**
      * Returns the current position of the file pointer.
      *
+     * This method will return the result of `fseek()` on the file handle.
+     *
+     *     $position = $reader->getPosition();
+     *
      * @return integer The current position.
      *
      * @throws Exception
      * @throws FileException If the size could not be read.
+     *
+     * @api
      */
     public function getPosition()
     {
@@ -103,12 +163,18 @@ class Reader
     }
 
     /**
-     * Returns the size of the file.
+     * Returns the byte size of the file.
      *
-     * @return integer The size of the file.
+     * This method will return the size of the file on disk as bytes.
+     *
+     *     $size = $reader->getSize();
+     *
+     * @return integer The size of the file in bytes.
      *
      * @throws Exception
      * @throws FileException If the size could not be read.
+     *
+     * @api
      */
     public function getSize()
     {
@@ -127,9 +193,15 @@ class Reader
      * determine the if the end of the file has been reached. If the EOF is not
      * reached, the file handle will be seeked back the one byte that was read.
      *
-     * @return boolean If the end of the file has been reached, `false` is
+     *     while (!$reader->isEndOfFile()) {
+     *         // not at the end yet
+     *     }
+     *
+     * @return boolean If the end of the file has been reached, `true` is
      *                 returned. If the end of the file has not been reached,
-     *                 `true` is returned.
+     *                 `false` is returned.
+     *
+     * @api
      */
     public function isEndOfFile()
     {
@@ -148,12 +220,20 @@ class Reader
     /**
      * Reads a specific number of bytes from the file.
      *
+     * This method will attempt to read the exact number of bytes specified.
+     * If the exact number of bytes could not be read, an exception will be
+     * thrown.
+     *
+     *     $data = $reader->read(10);
+     *
      * @param integer $bytes The number of bytes.
      *
      * @return string The bytes read.
      *
      * @throws Exception
      * @throws FileException If not all the bytes were read.
+     *
+     * @api
      */
     public function read($bytes)
     {
@@ -184,11 +264,17 @@ class Reader
     /**
      * Seeks to a specific point in the file.
      *
+     * This method will seek to the specified offset in the file.
+     *
+     *     $reader->seek(10, SEEK_CUR);
+     *
      * @param integer $offset The offset.
      * @param integer $whence The direction.
      *
      * @throws Exception
      * @throws FileException If seeking failed.
+     *
+     * @api
      */
     public function seek($offset, $whence = SEEK_SET)
     {
--- src/lib/Phine/Phar/File/Writer.php
+++ src/lib/Phine/Phar/File/Writer.php
@@ -8,7 +8,25 @@ use Phine\Phar\Exception\FileException;
 /**
  * Manages strict file writing operations.
  *
+ * Summary
+ * -------
+ *
+ * The `Writer` class provides a way to write files with heavy error checking.
+ * Instead of subtly triggering warnings, exceptions are thrown if something
+ * could not be done (e.g. fopen(), fwrite() a specific number of bytes, etc).
+ *
+ * Starting
+ * --------
+ *
+ * To start, you need to create a new instance of `Writer`.
+ *
+ *     use Phine\Phar\File\Writer;
+ *
+ *     $writer = new Writer('/path/to/file');
+ *
  * @author Kevin Herrera <kevin@herrera.io>
+ *
+ * @api
  */
 class Writer
 {
@@ -29,10 +47,23 @@ class Writer
     /**
      * Sets the path of the file for writing.
      *
+     * This method will set the path of the file to be written.
+     *
+     *     use Phine\Phar\File\Writer;
+     *
+     *     $writer = new Writer('/path/to/file');
+     *
+     * > Note that the file will not be opened until the first write operation
+     * > is attempted. An exception will still be thrown, however, if the
+     * > directory for the file does not exist, or if the file exists but is
+     * > not writable.
+     *
      * @param string $file The path of the file.
      *
      * @throws Exception
      * @throws FileException If the file path is not valid.
+     *
+     * @api
      */
     public function __construct($file)
     {
@@ -55,6 +86,18 @@ class Writer
 
     /**
      * Closes the file if it's open.
+     *
+     * This method will close the file handle if it has been opened.
+     *
+     *     use Phine\Phar\File\Writer;
+     *
+     *     $writer = new Writer('/path/to/file');
+     *
+     *     $reader->write($data);
+     *
+     *     unset($reader);
+     *
+     * @api
      */
     public function __destruct()
     {
@@ -66,7 +109,13 @@ class Writer
     /**
      * Returns the path of the file.
      *
+     * This method will return the path to the file being written.
+     *
+     *     $file = $writer->getFile();
+     *
      * @return string The path of the file.
+     *
+     * @api
      */
     public function getFile()
     {
@@ -74,12 +123,20 @@ class Writer
     }
 
     /**
-     * Returns the reading file handle.
+     * Returns the writing file handle.
+     *
+     * This method will return the writing file handle for the file.
+     *
+     *     $handle = $writer->getHandle();
+     *
+     * > Note that the file will be opened for writing binary data.
      *
      * @return resource The file handle.
      *
      * @throws Exception
      * @throws FileException If the file could not be opened for reading.
+     *
+     * @api
      */
     public function getHandle()
     {
@@ -95,11 +152,19 @@ class Writer
     /**
      * Writes a specific number of bytes to the file.
      *
+     * This method will attempt to write the exact string, or the length
+     * specified. If the whole string, or the specified length, could not
+     * be written, an exception will be thrown.
+     *
+     *     $writer->write($data);
+     *
      * @param string  $string The string to write to the file.
      * @param integer $length The number of bytes of the string to write.
      *
      * @throws Exception
      * @throws FileException If the file could not be written.
+     *
+     * @api
      */
     public function write($string, $length = null)
     {
